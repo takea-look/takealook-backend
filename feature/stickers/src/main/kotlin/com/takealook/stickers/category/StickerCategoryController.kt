e84b5647b335c87f650d00771ea522729803e905
easternkite <easternkite7@gmail.com>
2025-07-26 00:22:11 +0900

    [takea-look#4] data 이름 변경 icon -> sticker

--- feature/stickers/src/main/kotlin/com/takealook/stickers/category/StickerCategoryController.kt
+++ feature/stickers/src/main/kotlin/com/takealook/stickers/category/StickerCategoryController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/icon-categories")
+@RequestMapping("/sticker-categories")
 class StickerCategoryController @Autowired constructor(
     private val stickerCategoryRepository: StickerCategoryRepository
 ) {
