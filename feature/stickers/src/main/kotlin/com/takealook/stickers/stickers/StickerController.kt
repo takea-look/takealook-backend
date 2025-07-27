8c8c398521ef17447ced594c966aff44af0d1b9e
easternkite <easternkite7@gmail.com>
2025-07-28 01:38:04 +0900

    [takea-look#8] category id 별로 sticker 조회하도록 수정

--- feature/stickers/src/main/kotlin/com/takealook/stickers/stickers/StickerController.kt
+++ feature/stickers/src/main/kotlin/com/takealook/stickers/stickers/StickerController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -28,8 +29,14 @@ class StickerController @Autowired constructor(
     }
 
     @GetMapping
-    suspend fun getAllIcons(): ResponseEntity<List<Sticker>> {
-        val icons = iconRepository.findAll()
+    suspend fun getIcons(
+        @RequestParam(required = false) categoryId: Long?
+    ): ResponseEntity<List<Sticker>> {
+        val icons = if (categoryId != null) {
+            iconRepository.findByCategoryId(categoryId)
+        } else {
+            iconRepository.findAll()
+        }
         return ResponseEntity.ok(icons.toList())
     }
 }
--- feature/stickers/src/main/kotlin/com/takealook/stickers/stickers/StickerRepository.kt
+++ feature/stickers/src/main/kotlin/com/takealook/stickers/stickers/StickerRepository.kt
@@ -1,5 +1,11 @@
 package com.takealook.stickers.stickers
 
+import kotlinx.coroutines.flow.Flow
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
-interface StickerRepository : CoroutineCrudRepository<Sticker, Int>
+interface StickerRepository : CoroutineCrudRepository<Sticker, Int> {
+    /**
+     * Find all icons by category id.
+     */
+    suspend fun findByCategoryId(categoryId: Long): Flow<Sticker>
+}
