96366a56ef82c987e33476fd66295dac35bb9e10
easternkite <easternkite7@gmail.com>
2025-08-06 01:51:44 +0900

    Servlet Security 비활성화

--- app/src/main/kotlin/com/takealook/TakealookBackendApplication.kt
+++ app/src/main/kotlin/com/takealook/TakealookBackendApplication.kt
@@ -1,9 +1,16 @@
 package com.takealook
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = [
+        SecurityAutoConfiguration::class,
+        SecurityFilterAutoConfiguration::class
+    ]
+)
 class TakealookBackendApplication
 
 fun main(args: Array<String>) {
