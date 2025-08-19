1c1cdd08d4edad222698d6c4c3752113b0133275
easternkite <easternkite7@gmail.com>
2025-08-20 01:18:49 +0900

    [takea-look#34] WebSocket -> WebFlux 로 이전

--- feature/chat/build.gradle.kts
+++ feature/chat/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     implementation(projects.core.domain)
 
 //    implementation(libs.spring.boot.starter.data.redis)
-    implementation(libs.spring.boot.starter.websocket)
+    // webSocket 의존성은 필요없음 (webflux 자체적으로 지원하기 때문)
 }
 
 tasks.withType<Test> {
