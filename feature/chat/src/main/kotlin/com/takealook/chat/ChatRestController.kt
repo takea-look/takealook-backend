8436916bc07d7b28ef208fe55701fcdd2dedd70f
easternkite <easternkite7@gmail.com>
2025-08-22 23:59:58 +0900

    [takea-look#41] 채팅 > 메세지조회 API 추가

--- feature/chat/src/main/kotlin/com/takealook/chat/ChatRestController.kt
+++ feature/chat/src/main/kotlin/com/takealook/chat/ChatRestController.kt
@@ -1,21 +1,33 @@
 package com.takealook.chat
 
+import com.takealook.domain.chat.message.GetMessagesUseCase
 import com.takealook.domain.chat.room.GetChatRoomsUseCase
+import com.takealook.model.ChatMessage
 import com.takealook.model.ChatRoom
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/chat/rooms")
+@RequestMapping("/chat")
 class ChatRestController(
     private val getChatRoomsUseCase: GetChatRoomsUseCase,
+    private val getChatMessagesUseCase: GetMessagesUseCase
 ) {
 
-    @GetMapping
+    @GetMapping("/rooms")
     suspend fun getRooms(): ResponseEntity<List<ChatRoom>> {
         val rooms = getChatRoomsUseCase()
         return ResponseEntity.ok(rooms.toList())
+    }
+
+    @GetMapping("/messages")
+    suspend fun getMessages(
+        @RequestParam(required = true) roomId: Long
+    ): ResponseEntity<List<ChatMessage>> {
+        val messages = getChatMessagesUseCase(roomId)
+        return ResponseEntity.ok(messages)
     }
 }
